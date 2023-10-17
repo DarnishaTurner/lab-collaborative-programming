cd5e09470522d27741fb3e9ac9e3097cfdc9f990
DarnishaTurner <darnishaturner@pursuit.org>
2023-10-16 20:20:06 -0400

    Darnishas Finished Lab

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]} words - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+function sortByStringLength(words) {
+  return words.sort((a,b) => a.length - b.length)
+}
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +16,52 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(word) {
+  const wordLength = word.length;
+  const scrolledWords = [];
+
+  for (let i = 0; i < wordLength; i++) {
+    const scrolledWord = word.slice(i) + word.slice(0, i);
+    scrolledWords.push(scrolledWord);
+  }
+
+ const scrolledWord = scrolledWords.shift();
+ scrolledWords.push(scrolledWord);
+
+  return scrolledWords;
+}
+
+
+
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+
+function betweenExtremes(numbers) {
+  if (numbers.length === 0) {
+    return 0; // This line will return 0 for an empty array.
+  }
+
+  let allNumbers = true; // This line is to check if all elements are numbers
+
+  if (numbers.every((number) => typeof number === "number")) {
+    numbers.sort((a, b) => a - b);
+    const difference = numbers[numbers.length - 1] - numbers[0];
+    return difference;
+  } else {
+    allNumbers = false;
+  }
+
+  // If all elements are not numbers, below will return the original array
+  if (!allNumbers) {
+    return numbers;
+  }
+}
+
+
 
 /**
  * Returns the translation of English to morse code.
@@ -32,7 +72,37 @@ function betweenExtremes() {}
  * .- / -. . .-- / -- --- -. - ....
  * Hint: Check the morse-code-dictionary.json file to see what data is available.
  */
-function morseCodeTranslator() {}
+// function morseCodeTranslator(message, dictionary) {
+//   let morseMessage = "";
+
+//   for (let i = 0; i < message.length; i++) {
+//     let letter = message[i].toUpperCase();
+//     if (dictionary[letter]) {
+//       morseMessage += dictionary[letter] + " ";
+//     } else if (letter === " ") {
+//       morseMessage += " ";
+//     } else {
+//       morseMessage += "? ";
+//     }
+//   }
+
+//   return morseMessage.trim();
+// }
+
+function morseCodeTranslator(message, dictionary) {
+  const morseMessage = message
+    .toUpperCase() 
+    .split('')
+    .filter((char) => char !== " ")
+    .map((char) => {
+       if (dictionary[char]) {
+        return dictionary[char];
+      } 
+    })
+    .join(' ');
+
+  return morseMessage;
+}
 
 module.exports = {
   sortByStringLength,
